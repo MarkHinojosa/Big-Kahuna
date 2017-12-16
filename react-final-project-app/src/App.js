b9631ce930d117583c3dc262dd40a294615297be
gormangit <contact@jongorman.top>
2017-12-16 11:49:19 -0600

    coffee: morning of today

--- react-final-project-app/src/App.js
+++ react-final-project-app/src/App.js
@@ -4,87 +4,148 @@ import React, {Component} from 'react';
 // import {Rectangle} from 'react-shapes';
 import './App.css';
 import avatar from './daftCat.png';
+import matchedAvatar from './default.png';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentTurn: 1,
-      profiles: [{
-          shown: "",
-          url: "",
-      }],
+    constructor() {
+        super();
+        this.state = {
+            currentTurn: 1,
+            profiles: [{
+                shown: "",
+                url: "",
 
-    };
-    this.handleOnClick = this.handleOnClick.bind(this)
-    const testCopy = this.state.profiles;
-  }
-  componentDidMount() {
-    fetch('https://randomuser.me/api/?results=15')
-    .then((response) => response.json())
-    .then((responseJson) => responseJson.results)
-    .then(newArr => newArr.concat(newArr))
-    .then(doubledArr => doubledArr.sort(() => Math.random() * 2 - 1))
-    .then(results => {
-      return results.map((profile) => {
-        return ({
-          url: profile.picture.large,
-          shown: avatar,
+            }],
+
+        };
+        this.baseState = []
+        this.handleOnClick = this.handleOnClick.bind(this)
+    }
+    componentDidMount() {
+        fetch('https://randomuser.me/api/?results=4')
+            .then((response) => response.json())
+            .then((responseJson) => responseJson.results)
+            .then(newArr => newArr.concat(newArr))
+            .then(doubledArr => doubledArr.sort(() => Math.random() * 2 - 1))
+            .then(results => {
+                return results.map((profile) => {
+                    return ({
+                        url: profile.picture.large,
+                        shown: avatar,
+                    })
+                })
+            })
+            .then(doubledArr  => {
+                this.setState({ profiles: doubledArr})
+                this.baseState = doubledArr
+            })
+    }
+
+    handleOnClick = (data, index) => {
+
+        if(this.state.currentTurn === 1){
+
+            const copy = [...this.state.profiles];
+            copy[index].shown = data.url;
+            this.setState({ profiles: copy })
+            this.setState({ currentTurn: 2 })
+            //why does baseState change???
+            console.log(this.state.profiles[index].shown)
+            console.log(this.state.currentTurn)
+
+        }
+        if(this.state.currentTurn === 2 && this.state.profiles[index].shown === avatar){
+          console.log('58')
+            //shownArrs is mapping through the profiles and making an array of just
+            //the shown values
+            const shownArrs = this.state.profiles.map((profile) => profile.shown)
+            this.setState({currentTurn: 1})
+            if(shownArrs.includes(data.url)){
+                //if the new clicked is found in the shownArrs
+                const matchedCopy = [...this.state.profiles];
+                matchedCopy[index].shown = matchedAvatar;
+                // map through the profiles and find something that is showing
+                // a picture
+                this.state.profiles.map((profile) => {
+                    if(profile.shown !== avatar){
+                        return profile.shown = matchedAvatar
+                    }
+                })
+                this.setState({ profiles: matchedCopy })
+                //updating the profiles state to store the new matches
+                alert("match")
+                this.checkForWin();
+
+            } else {
+                const copy = [...this.state.profiles];
+                copy[index].shown = data.url;
+                this.setState({ profiles: copy })
+                this.state.profiles.map((currentProfile) => {
+                    if(currentProfile.shown !== avatar){
+                        setTimeout(function(){
+                            return currentProfile.shown = avatar}, 100)
+                    }
+                })
+                this.setState({currentTurn: 1})
+
+            }
+        }
+    }
+    checkForWin(data) {
+        let winningNum = 0
+      console.log('Is this working')
+
+      const win = [...this.state.profiles];
+      win.map((wins) => {
+            if(wins.shown === matchedAvatar) {
+              winningNum ++;
+                console.log(winningNum)
+            }
+            if(winningNum === win.length) {
+              alert("Game Won!")
+          }
+
         })
-      })
-    })
-    .then(doubledArr => this.setState({ profiles: doubledArr }))
-  }
 
-  handleOnClick = (data, index) => {
-    const that = this;
-    const shownArr = this.state.profiles.map((profileIndex) => profileIndex.shown);
-    const copy = [...that.state.profiles];
-    if(this.state.currentTurn === 1){
-      let localCopy = copy;
-      if(this.state.profiles[index].shown === avatar){
-        localCopy[index].shown = this.state.profiles[index].url;
-        this.setState({ profiles: localCopy })
-        this.setState({currentTurn: 2})
-        this.setState({copyShown: copy1})
 
-      }
     }
-    if(this.state.currentTurn === 2 && this.state.profiles[index].shown === avatar ){
-      if(shownArr.includes(data.url)){
-        let localCopy = copy;
-        localCopy[index].shown = this.state.profiles[index].url;
-        this.setState({ profiles: localCopy })
-        alert("MATCH!")} else {
-          //need to reset state back to original
-          console.log("line 57")
-      };
-    }
-}
-  renderprofiles() {
-    const {profiles} = this.state;
-    if (profiles.length) {
-      return (
-        profiles.map((obj, key) => {
-          return (
-            <div key={key}>
-              <img src={profiles[key].shown} onClick={() => this.handleOnClick(obj, key)}/>
-             </div>
-          )
-        })
-      )
-    }
-  }
 
-  render() {
-    return (
-      <div className="ui container">
-        <div className="body">
-          {this.renderprofiles()}
-        </div>
-      </div>
-    )
-  }
+
+    renderAlert() {
+        return(
+
+            <div>
+              <h1> {"not a match"} </h1>
+            </div>
+        )
+    }
+
+    renderprofiles() {
+        const {profiles} = this.state;
+
+        return (
+            profiles.map((obj, key) => {
+                return (
+                    <div key={key}>
+                      <img alt={avatar} src={profiles[key].shown} onClick={() =>
+                          this.handleOnClick(obj, key)}/>
+                        {obj.url}
+                    </div>
+                )
+            })
+        )
+    }
+
+    render() {
+
+        return (
+            <div className="ui container">
+              <div className="body">
+                  {this.renderprofiles()}
+              </div>
+            </div>
+        )
+    }
 }
 
 export default App;
